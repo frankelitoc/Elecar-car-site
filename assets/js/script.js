4f5b97c76344bf5765a9a2399af194770f326d50
rasan3-1416 <md.rakibhasan3.1416@gmail.com>
2023-10-19 19:49:25 +0600

    build: Complete header/nav section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,25 @@
 /*=============== SHOW MENU ===============*/
-
-
+const navMenu = document.getElementById('nav-menu'),
+      navClose = document.getElementById('nav-close'),
+      navToggle = document.getElementById('nav-toggle')
+// Menu Show
+if(navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
+// Menu Hide
+if(navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
 /*=============== REMOVE MENU MOBILE ===============*/
-
-
+const navLink = document.querySelectorAll('.nav__link')
+function linkAction() {
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(eachLink => eachLink.addEventListener('click', linkAction))
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 
 
